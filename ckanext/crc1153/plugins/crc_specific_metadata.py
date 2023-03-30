3056de7d19e970ae41a51a1dfe9ba92476a6de8a
pooya.oa <pooya.oladazimi@tib.eu>
2023-03-30 15:00:44 +0200

    added template directory for specifc metadata plugin

--- ckanext/crc1153/plugins/crc_specific_metadata.py
+++ ckanext/crc1153/plugins/crc_specific_metadata.py
@@ -9,8 +9,10 @@ class CrcSpecificMetadata(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
-        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_template_directory(config_, 'templates/crc_specific_metadata')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('public/crc_specific_metadata', 'ckanext-crc-specific-metadata')
+        toolkit.add_resource('public/crc_specific_metadata', 'ckanext-crc1153-specific-metadata')
+    
+    
     
     
