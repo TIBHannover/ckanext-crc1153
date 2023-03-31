b64aff460c3ba3fb3753c6cabef0ed7f2d681d62
pooya.oa <pooya.oladazimi@tib.eu>
2023-03-31 09:44:18 +0200

    set a general template and public for all plugins

--- ckanext/crc1153/plugins/layout.py
+++ ckanext/crc1153/plugins/layout.py
@@ -13,7 +13,7 @@ class CrcLayoutPlugin(plugins.SingletonPlugin):
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public/crc_layout')
+        toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('public/crc_layout', 'ckanext-crc1153-layout')
     
 
